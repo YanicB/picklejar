09afc250eebc191392c9f2a70f05d2cf7820e1e8
YanicB <yanic.ballez@gmail.com>
2025-11-02 12:59:05 -0500

    implement svg

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,16 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import PickleJarSvg from "../components/PickleJarSvg";
+
 const MainPage = () => {
     return (
-        <section className="w-full min-h-screen flex flex-column items-center justify-center py-12 px-4">
-            <div className="max-w-2xl mx-auto w-full space-y-6 flex flex-col items-center justify-center">
-                <p className="text-xl font-bold">in a pickle?</p>
-                <Link to="/create" className="font-bold bg-green-500 text-center px-6 py-4 rounded-xl hover:bg-green-600">
-                    Get Started
-                </Link>
+        <section className="w-full min-h-screen flex flex-col items-center justify-center py-12 px-4">
+            <div className="relative w-[min(90vw,720px)]">
+                <PickleJarSvg className="block w-full h-auto" />
+                <div
+                    className="absolute flex flex-col items-center justify-center text-center gap-3 pointer-events-none"
+                    style={{
+                        left: "23%",   // tweak these 4 numbers to fit your label
+                        top: "44%",
+                        width: "54%",
+                        height: "32%",
+                    }}
+                >
+                    <p className="text-xl font-bold m-0">in a pickle?</p>
+                    <Link
+                        to="/create"
+                        className="font-bold bg-green-500 text-white px-6 py-3 rounded-xl hover:bg-green-600 pointer-events-auto"
+                    >
+                        Get Started
+                    </Link>
+                </div>
             </div>
         </section>
-    )
+    );
+};
 
-}
-
-export default MainPage
+export default MainPage;
