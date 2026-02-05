40bde2e272d04b33e92fddc6451203a8a79e0611
YanicB <yanic.ballez@gmail.com>
2026-02-05 00:58:19 -0500

    add footer to application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,21 @@ import MainPage from './pages/MainPage'
 import CreatePoll from './pages/CreatePoll'
 import About from './pages/About'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 import How from './pages/How'
 import PollsPage from './pages/PollsPage'
 import { Routes, Route, Outlet } from 'react-router-dom'
 
 const Layout = () => {
     return (
-        <div className="overflow-hidden bg-[#F8F6F1] h-screen">
+        <div className="flex flex-col min-h-screen bg-[#F8F6F1]">
             <header>
                 <NavBar />
             </header>
-            <main >
+            <main className="flex-grow">
                 <Outlet />
             </main>
+            <Footer />
         </div>
     )
 }
